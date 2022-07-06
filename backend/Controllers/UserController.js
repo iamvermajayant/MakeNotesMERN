33ae19a6dedd1288958fb18fad4038fbcc4721dc
Jayant Verma <iamvermajayant@gmail.com>
2022-07-06 22:37:02 +0530

    Note Route Fixed

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,57 +1,96 @@
-const User = require('../Model/userModel');
-const asyncHandler = require('express-async-handler');
-const generateToken = require('../utilis/generateToken');
+// import asyncHandler from "express-async-handler";
+const asyncHandler = require("express-async-handler");
+// import User from "../models/userModel.js";
+const User = require("../models/userModel.js");
+// import generateToken from "../utils/generateToken.js";
+const generateToken = require("../utils/generateToken.js");
 
-const registerUser = asyncHandler (async (req,res) => {
-    const {name, email, password, pic} = req.body;
+//@description     Auth the user
+//@route           POST /api/users/login
+//@access          Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    const userExist = await User.findOne({email});
+  const user = await User.findOne({ email });
 
-    if(userExist){
-        res.status(404);
-        throw new Error('user already exists');
-    }
-
-    const user = await User.create({name, email, password, pic});
-
-    if(user){
-        res.status(201).json({
-            _id : user._id,
-            name : user.name,
-            email : user.email,
-            isAdmin : user.isAdmin,
-            pic : user.pic,
-            token : generateToken(user._id),
-        });
-    }
-    else {
-        res.status(400);
-        throw new Error('Error occured');
-    }
-
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
 });
 
-const authUser = asyncHandler (async (req,res) => {
-    const { email, password} = req.body;
+//@description     Register new user
+//@route           POST /api/users/
+//@access          Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
 
-    const user = await User.findOne({email});
+  const userExists = await User.findOne({ email });
 
-    if(user && (await user.matchPassword(password))) {
-        res.json({
-            _id : user._id,
-            name : user.name,
-            email : user.email,
-            isAdmin : user.isAdmin,
-            pic : user.pic,
-            token : generateToken(user._id),
-        });
-    }
-    else{
-        res.status(400);
-        throw new Error('Invalid email or password');
-    }
+  if (userExists) {
+    res.status(404);
+    throw new Error("User already exists");
+  }
 
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
 });
 
+// @desc    GET user profile
+// @route   GET /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
-module.exports = {registerUser , authUser };
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.pic = req.body.pic || user.pic;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      pic: updatedUser.pic,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+module.exports = { authUser, updateUserProfile, registerUser };
--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const { registerUser , authUser } = require('../Controllers/UserController');
-
+// import express from "express";
+const express = require("express");
+const {authUser,
+  registerUser,
+  updateUserProfile,} = require('../controllers/userController.js');
+// import {
+//   authUser,
+//   registerUser,
+//   updateUserProfile,
+// } from "../controllers/userController.js";
+// import { protect } from "";
+const {protect} = require('../middleware/authMiddleware.js');
 const router = express.Router();
 
-router.route('/').post(registerUser)
-router.route('/login').post(authUser)
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router.route("/profile").post(protect, updateUserProfile);
 
-
-module.exports = router;
+module.exports =  router;
